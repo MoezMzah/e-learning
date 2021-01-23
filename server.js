c181cd5bc843037c2b4195f5b7dcb7d0c2d18c75
moez mzeh <moez.mzah@gmail.com>
2021-01-23 03:55:44 +0100

    deployment

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path=require('path')
 
 const app = express();
 //connect Database
 connectDB();
 
-app.get("/", (req, res) => res.send("API running"));
+
 
 // init middleware
 app.use(express.json({ extended: false }));
@@ -25,6 +26,11 @@ const path = require("path");
 
 app.use(cors());
 app.use(fileRoute);
-
+if (process.env.NODE_ENV==='production'){
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    });
+}
 const PORT = process.env.POPRT || 5000;
 app.listen(PORT, (err) => (err ? console.error(err) : console.log(`🚀 is 🏃 on port ${PORT} `)));
